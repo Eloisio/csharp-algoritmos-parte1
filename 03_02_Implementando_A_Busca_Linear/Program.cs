d171b430ff9901b1752d038249145862f8875ba5
Joel Rodrigues <joel.lima@engenhos.net>
2021-01-28 09:16:09 -0300

    03.02 - Implementando a busca linear

---
+++ 03_02_Implementando_A_Busca_Linear/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _03_02_Implementando_A_Busca_Linear
+{
+    class Program
+    {
+        static int BuscarItem(int[] array, int itemBuscado)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+                if (array[i] == itemBuscado)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            var itens = new int[]{ 5, 10, 15, 20, 25 };
+
+            var itemBuscado = 20;
+
+            // for(int i = 0; i < itens.Length; i++)
+            // {
+            //     Console.WriteLine(itens[i]);
+
+            //     if(itens[i] == itemBuscado)
+            //     {
+            //         Console.WriteLine($"Posição do item buscado: {i}");
+            //         break;
+            //     }
+            // }
+
+            Console.WriteLine(BuscarItem(itens, itemBuscado));
+        }
+    }
+}
